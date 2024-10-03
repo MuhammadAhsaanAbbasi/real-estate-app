19542ef7a478064ac49beffa8819ce4fa39c3ee6
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-10-03 10:04:37 +0500

    Project Setup

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Button>
+        Hello Real Estate
+      </Button>
+    </div>
+  );
+}
